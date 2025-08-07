794d15630ebaca6bae58f1236b120b9ea749c4a2
FrederickKisife <kilafrederick@gmail.com>
2025-08-07 02:43:27 -0700

    setting up my server with express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
+const express = require("express")
+const app =express()
 
+//routes
+app.get("/",(req, res)=>{
+    res.send("Api created")
+})
+
+const api = API_URL || 5000
+
+app.listen(api, ()=>{
+    console.log(`Server Running on port :${api}`);
+    
+})
